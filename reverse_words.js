04547d696b2972ade1739bcaa61b7b2ff651e0df
Valera-Neg <nehovora.val@gmail.com>
2022-08-03 12:58:46 -0400

    uncommented last join()

--- reverse_words.js
+++ reverse_words.js
@@ -1,5 +1,5 @@
 function reverseWords(str) {
-  return str.split('').reverse().join('').split(' ')//.reverse().join(' ')
+  return str.split('').reverse().join('').split(' ').reverse().join(' ')
    
 }
 
